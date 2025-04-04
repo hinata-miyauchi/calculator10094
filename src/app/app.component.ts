029bca2a14bd9e6a71eb2b487bb549a0dbd05c73
hinata-miyauchi <hinata.miyauchi@pathoslogos.co.jp>
2025-04-04 12:08:15 +0900

    初回

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CalculatorComponent } from '../app/calculator/calculator.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, CalculatorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'cal-app';
---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  imports: [],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+  display: string = '';
+  result: string = '';
+
+  onButtonClick(buttonValue: string): void {
+    if (buttonValue === '=') {
+      try {
+        this.result = 'Answer:  ' + this.calculate(this.display);
+      } catch (error) {
+        this.result = 'Error'; 
+      }
+    } else if (buttonValue === 'AC') {
+      this.display = '';
+      this.result = '';
+    } else if (buttonValue === 'DEL') {
+      this.deleteLastCharacter();
+    } else {
+      this.display += buttonValue;
+      this.display = this.display.replace(/\*/g, '×');
+      this.display = this.display.replace(/\//g, '÷');
+    }
+  }
+
+  calculate(expression: string): string {
+    try {
+      const result = this.evaluateExpression(expression);
+      return this.formatResult(result);
+    } catch (e) {
+      throw new Error('Invalid expression');
+    }
+  }
+
+  evaluateExpression(expression: string): number {
+    expression = expression.replace(/×/g, '*').replace(/÷/g, '/');
+    return new Function('return ' + expression)();
+  }
+
+  formatResult(result: number): string {
+    if (Math.abs(result) >= 10000000000) {
+      throw new Error('Result exceeds 10 billion');
+    }
+    const roundedResult = result.toFixed(8);
+    return parseFloat(roundedResult).toString(); 
+  }
+
+  deleteLastCharacter() {
+    this.display = this.display.slice(0, -1);
+  }
+}
